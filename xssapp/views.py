4069351ba1e6ad9ad467fb7e45b6b8a2bf30ba16
applepeerke <peterheijligers@solcon.nl>
2023-08-09 16:24:43 +0200

    New django version

--- xssapp/views.py
+++ xssapp/views.py
@@ -5,5 +5,7 @@ from .models import Post
 def post_list(request):
     # posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     posts = Post.objects.order_by('published_date')
-
+    # [PHe] Force a security violation (not tested/working)
+    for p in Post.objects.raw( f"SELECT * FROM post WHERE title = {request.data.title}" ):
+        print( p )
     return render(request, 'xssapp/post_list.html', {'posts': posts})
